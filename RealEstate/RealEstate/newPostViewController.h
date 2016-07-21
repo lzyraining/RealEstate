e38534aa24baa1dfe026be974682c9a05b762fc8
kiwiecho <shuting@Shutings-MacBook-Pro.local>
2016-07-21 14:51:21 -0500

    add newpost controller .h .m files

---
+++ RealEstate/RealEstate/newPostViewController.h
@@ -0,0 +1,13 @@
+//
+//  newPostViewController.h
+//  RealEstate
+//
+//  Created by Shuting lang on 7/21/16.
+//  Copyright © 2016 Zhuoyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface newPostViewController : UIViewController
+
+@end
